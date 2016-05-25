3727ff5a9644f147cce2bd07e63e6a8e33ee95b4
IRIS <iriszero5@gmail.com>
2016-05-25 10:00:29 +0900

    Add Circular Queue

---
+++ core/circular_queue.c
@@ -0,0 +1,68 @@
+#include "circular_queue.h"
+#include <stdlib.h>
+#include <assert.h>
+
+int cq_init(CircularQueue* cq, int size) {
+    if (cq != NULL) cq_destroy(cq);
+    cq = (CircularQueue*)malloc(sizeof(CircularQueue));
+    if (cq == NULL) return ALLOCATION_FAIL;
+    cq->key = (cq_element_t*) malloc(sizeof(cq_element_t) * size);
+    cq->first = 1;
+    cq->rear = 0;
+    cq->cnt= 0;
+    cq->capacity = size;
+    return SUCCESS;
+
+}
+int cq_is_empty(CircularQueue* cq) {
+    assert(cq);
+    return cq->cnt == 0;
+}
+int cq_is_full(CircularQueue* cq) {
+    assert(cq);
+    return cq->capacity == cq->cnt;
+}
+int cq_pop(CircularQueue* cq) {
+    assert(cq);
+    if (cq_is_empty(cq)) {
+        return EMPTY;
+    }
+    else {
+        cq->first = (cq->first + 1) % (cq->capacity);
+        cq->cnt--;
+    }
+    return SUCCESS;
+}
+int cq_push(CircularQueue* cq, cq_element_t num) {
+    assert(cq);
+    if (cq_is_full(cq)) {
+        return FULL;
+    }
+    else {
+        cq->cnt++;
+        cq->rear = (cq->rear + 1) % (cq->capacity);
+        cq->key[cq->rear] = num;
+    }
+    return SUCCESS;
+}
+cq_element_t cq_front (CircularQueue* cq) {
+    if (cq_is_empty(cq)) {
+        return EMPTY;
+    }
+    else {
+        return cq->key[cq->first];
+    }
+}
+
+cq_element_t cq_rear (CircularQueue* cq) {
+    if (cq_is_empty(cq)) {
+        return EMPTY;
+    }
+    else {
+        return cq->key[cq->rear];
+    }
+}
+void cq_destroy(CircularQueue* cq ) {
+    free(cq->key);
+    free(cq);
+}
---
+++ core/circular_queue.h
@@ -0,0 +1,24 @@
+#pragma once
+
+typedef int cq_element_t;
+static const int SUCCESS = 0;
+static const int EMPTY = 10;
+static const int FULL = 11;
+static const int ALLOCATION_FAIL = 14;
+struct CircularQueue {
+    cq_element_t *key;
+    int first;
+    int rear;
+    int cnt;
+    int capacity;
+};
+
+typedef struct CircularQueue CircularQueue;
+int cq_init( CircularQueue* cq, int size );
+int cq_is_empty( CircularQueue* cq );
+int cq_is_full( CircularQueue* cq );
+int cq_push( CircularQueue* cq , cq_element_t num);
+int cq_pop( CircularQueue* cq );
+cq_element_t cq_front (CircularQueue* cq);
+cq_element_t cq_rear (CircularQueue* cq);
+void cq_destroy (CircularQueue* cq );
