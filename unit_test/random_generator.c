34e4d04c82ff369a24edbf3b4e988c4e05955064
IRIS <iriszero5@gmail.com>
2016-05-26 21:58:07 +0900

    Fix logical error that random_generator does not return the "max" value

--- unit_test/random_generator.c
+++ unit_test/random_generator.c
@@ -1,9 +1,9 @@
 #include "random_generator.h"
 int get_random_int(int min, int max) {
     assert(min <= max);
-    int diff = max - min, res;
-    srand(time(NULL));
-    if (diff < RAND_MAX) {
+    int diff = max - min + 1, res;
+
+    if (diff > RAND_MAX) {
         res = min + (diff / RAND_MAX) * get_random_int(0, RAND_MAX) + get_random_int(0, diff % RAND_MAX);
     }
     else {
